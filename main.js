353d31b69350aeae57685d224654959e11b6edaf
jimbo <jbear.um@gmail.com>
2022-06-10 17:32:07 +0900

    fix

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ const hideEob = () => {
   for (let i = 1; i < 10; i++) {
     const tgt = document.getElementById(`eob_${i}`);
     if (tgt) {
+      tgt.style.display = 'none';
       const idiv = tgt.parentNode;
-      idiv.style.display = 'none';
+      // idiv.style.display = 'none';
       idiv.parentNode.parentNode.style.height = 'auto';
+      idiv.parentNode.parentNode.parentNode.style.height = 'auto';
     } else {
       return;
     }
   }
 }
+
 
 hideEob();
